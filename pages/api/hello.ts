b41e4df65ab85cb728aed30794a1f0ddd86da89d
odaneo <odaneo@outlook.com>
2022-06-08 23:48:17 +0800

    feat: add mockjs

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import { mock } from 'mockjs'
+console.log(mock)
 type Data = {
   name: string
 }
@@ -9,5 +10,13 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    mock({
+      'list|1-10': [
+        {
+          'id|+1': 1,
+        },
+      ],
+    })
+  )
 }
